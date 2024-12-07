1dcc37aba85200786ee0553208078a369663683e
Davide Di Francesco <davdifr@gmail.com>
2024-12-07 12:07:02 +0100

    Initialization of the main pages and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component'),
+  },
+  {
+    path: 'bar',
+    loadComponent: () => import('./pages/bar/bar.component'),
+  },
+  {
+    path: 'beers',
+    loadComponent: () => import('./pages/beers/beers.component'),
+  },
+  {
+    path: 'events',
+    loadComponent: () => import('./pages/events/events.component'),
+  },
+  {
+    path: 'card',
+    loadComponent: () => import('./pages/card/card.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
